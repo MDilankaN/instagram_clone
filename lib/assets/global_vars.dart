c47c1fc6cf214ebf67c93c34458cc80daa7fa5e9
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2024-01-20 23:27:51 +0530

    import add post screen

--- lib/assets/global_vars.dart
+++ lib/assets/global_vars.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/add_post_screen.dart';
 
 const webScreenSize = 600;
 
 const List<Widget> homeScreenItems = [
   Text('Feed'),
   Text('Search'),
-  Text('Add post'),
+  AddPostScreen(),
   Text('Notification'),
   Text('Profile'),
 ];
